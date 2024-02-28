d0e72546e97a7fd9d328ac7e8310c3303c36c4ab
barmajaweb <156531247+barmajaweb@users.noreply.github.com>
2024-02-28 12:02:31 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,31 @@
+let img =document.querySelector('.img1');
+let content =document.querySelector('.content');
+let wail = document.querySelector('.adam');
+let cava = document.querySelector('.img1');
+let abah = document.querySelector('.content');
+let anah = document.querySelector('.amin')
+function phones(x){
+    img.src = x;
+}
+function colors(color){
+    content.style.background = color;
+    
+}
+function acaf(x){
+    wail.style.background = x;
+   
+    
+    
+}
+
+function amj(afa){
+    wail.style.color = afa;
+    
+}
+function adam(c){
+    cava.src = c;
+
+}
+function mama(s){
+    abah.style.background = s;
+}
